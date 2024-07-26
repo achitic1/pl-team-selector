7ee7467b20384ce8fbfced0ace1e35c27ee7d3a1
achitic1 <chitianthony52@gmail.com>
2024-07-25 22:33:19 -0400

    Add images for each team to the team card

--- scripts/results.js
+++ scripts/results.js
@@ -1,19 +1,20 @@
 class Team {
-    constructor(name, trophies, colors, founded, location, stadium) {
+    constructor(name, trophies, colors, founded, location, stadium, image) {
       this.name = name;
       this.trophies = trophies;
       this.colors = colors;
       this.founded = founded;
       this.location = location;
       this.stadium = stadium;
+      this.image = image;
     }
 }
 
-const teams = [ new Team("Man City", 35, "skyblue", 1880, "Manchester, England", "Etihad Stadium"),
-                new Team("Man United", 68, "red", 1878, "Manchester, England", "Old Trafford"),
-                new Team("Chelsea", 34, "blue", 1905, "London, England", "Stamford Bridge"),
-                new Team("Liverpool", 69, "red", 1892, "Liverpool, England", "Anfield"),
-                new Team("Aston Villa", 25, "maroon", 1874, "Birmingham, England", "Villa Park")
+const teams = [ new Team("Man City", 35, "skyblue", 1880, "Manchester, England", "Etihad Stadium", "man-city.jpg"),
+                new Team("Man United", 68, "red", 1878, "Manchester, England", "Old Trafford", "man-united.jpg"),
+                new Team("Chelsea", 34, "blue", 1905, "London, England", "Stamford Bridge", "chelse,jpg"),
+                new Team("Liverpool", 69, "red", 1892, "Liverpool, England", "Anfield", "liverpool.jpg"),
+                new Team("Aston Villa", 25, "maroon", 1874, "Birmingham, England", "Villa Park", "aston-villa.jpg")
             ];
 
 function generateResult(bestCategory) {
@@ -47,20 +48,38 @@ function generateResult(bestCategory) {
     let teamCard = document.createElement("div");
     teamCard.classList.add("team-card");
 
+    // Containers to separate data and image
+    let statContainer = document.createElement("div");
+    let imageContainer = document.createElement("div");
+    statContainer.classList.add("stats-container");
+    imageContainer.classList.add("image-container");
+
     for (const [key, value] of Object.entries(matchedTeam)) {
-        let data = document.createElement("h2");
+        if (key !== "image") {
+            let data = document.createElement("h2");
 
-        let keySpan = document.createElement("span");
-        keySpan.textContent = key + ": ";
-        
-        let valueSpan = document.createElement("span");
-        valueSpan.textContent = value;
-        valueSpan.style.color = matchedTeam.colors;
+            let keySpan = document.createElement("span");
+            keySpan.textContent = key + ": ";
+            
+            let valueSpan = document.createElement("span");
+            valueSpan.textContent = value;
+            valueSpan.style.color = matchedTeam.colors;
 
-        data.appendChild(keySpan);
-        data.appendChild(valueSpan);
-        teamCard.append(data);
+            data.appendChild(keySpan);
+            data.appendChild(valueSpan);
+            statContainer.appendChild(data);
+        } else {
+            // Create and add the image
+            let image = document.createElement("img");
+            image.src = "team-photos/" + value; // Set the path
+            image.alt = matchedTeam.name + " stadium";
+            image.classList.add("team-image");
+            imageContainer.appendChild(image);
+        }
     }
+
+    teamCard.appendChild(statContainer);
+    teamCard.appendChild(imageContainer);
 
     results.appendChild(teamCard);
 
