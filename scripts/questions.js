c4730dcc770bee81b04ba0f8bf1c594bb508ea4c
achitic1 <chitianthony52@gmail.com>
2024-07-29 14:27:57 -0400

    Fix bugs and refactor code
    Fix the bug where you can show new questions by changing the answer of questions that are not the current one

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,4 +1,8 @@
 /* This script loads the questions on to the page */
+
+// Keeps track of the question the user is answering
+let currQuestion = 0;
+
 (function() {
   function createQuestions() {
     const questionsList = document.querySelector("form");
@@ -22,18 +26,18 @@
     ]);
 
     for (let i = 0; i < questions.length; i++) {
-      let newQuestion = document.createElement("fieldset");
+      const newQuestion = document.createElement("fieldset");
 
       // Create the question title
-      let questionTitle = document.createElement("h2");
+      const questionTitle = document.createElement("h2");
       questionTitle.textContent = `${i + 1}. ${questions[i]}`;
       newQuestion.appendChild(questionTitle);
 
       // Create the options for answers
       for (let j = 1; j <= 3; j++) {
-        let label = document.createElement("label");
+        const label = document.createElement("label");
         
-        let input = document.createElement("input");
+        const input = document.createElement("input");
         input.setAttribute("type", "radio");
         input.setAttribute("name", `question-${i}`);
         input.setAttribute("value", j);
@@ -44,12 +48,23 @@
         newQuestion.appendChild(document.createElement("br"));
       }
 
+      // Hides the questions from the user and only reveals if they answer the previous question
       newQuestion.classList.add("hidden");
+      newQuestion.addEventListener("change", () => {
+        const answers = questionsList.children[currQuestion].querySelectorAll(`input[type="radio"]`);
+        const isAnswered = Array.from(answers).some(answer => answer.checked);
+
+        if (isAnswered) {
+          questionsList.children[currQuestion + 1].classList.remove("hidden");
+          currQuestion++;
+        }
+      });
+      
       questionsList.appendChild(newQuestion);
     }
 
     // Creating and hiding the submit button
-    let submitButton = document.createElement("button");
+    const submitButton = document.createElement("button");
     submitButton.type = "submit";
     submitButton.textContent = "Submit";
     submitButton.classList.add("hidden");
@@ -58,3 +73,11 @@
 
   document.addEventListener('DOMContentLoaded', createQuestions);
 })();
+
+// Start button to show the first question and disappear when clicked
+const startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", () => {
+    const firstQuestion = document.querySelector("fieldset");
+    firstQuestion.classList.remove("hidden");
+    startButton.classList.add("hidden");
+;});
